27d2d5e37ef2558b83a2fb55df3c18c8460311e9
Harpreet <harpreet@smartitventures.com>
2020-01-10 17:50:16 +0530

    added login/Registation Functionality

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message.php');
+		echo $this->session->userdata('isUserLoggedIn'); 
+		//$this->load->view('welcome_message.php');
 	}
 }
